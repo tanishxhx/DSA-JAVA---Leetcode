5711b9048af0ce986bfd68b8e7f9fd3f250630f3
tanishxhx <tanishsingh7022@gmail.com>
2026-05-25 18:02:53 +0530

    Time: 1 ms (94.12%) | Memory: 43.7 MB (41.06%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -3,8 +3,8 @@ class Solution {
         if(!(str1 + str2).equals(str2 + str1)){
             return "";
         }
-        int gcdLen = gcd(str1.length(), str2.length());
-        return str1.substring(0, gcdLen);
+        int lengthOfGCD = gcd(str1.length(), str2.length());
+        return str1.substring(0, lengthOfGCD);
     }
     public int gcd(int a, int b){
         return b == 0? a : gcd(b, a % b);
